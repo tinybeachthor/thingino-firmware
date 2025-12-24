9180e8d7fb7e8b2c2e6fdb2d6efc571629e0a16f
Martin Toman <25009432+tinybeachthor@users.noreply.github.com>
2025-12-24 17:41:39 -0600

    nix flake init

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Example flake environment for build buildroot projects";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    # https://flake.parts/module-arguments.html
+    flake-parts.lib.mkFlake { inherit inputs; } (top@{ config, withSystem, moduleWithSystem, ... }: {
+      imports = [
+        # Optional: use external flake logic, e.g.
+        # inputs.foo.flakeModules.default
+      ];
+      flake = {
+        # Put your original flake attributes here.
+      };
+      systems = [
+        # systems for which you want to build the `perSystem` attributes
+        "x86_64-linux"
+        # ...
+      ];
+      perSystem = { config, pkgs, ... }: {
+        # Recommended: move all package definitions here.
+        # e.g. (assuming you have a nixpkgs input)
+        # packages.foo = pkgs.callPackage ./foo/package.nix { };
+        # packages.bar = pkgs.callPackage ./bar/package.nix {
+        #   foo = config.packages.foo;
+        # };
+        devShells.default = (pkgs.buildFHSEnv {
+          name = "buildroot";
+          targetPkgs = pkgs: (with pkgs;
+            [
+              (lib.hiPrio gcc)
+              file
+              gnumake
+              ncurses.dev
+              pkg-config
+              unzip
+              wget
+              pkgsCross.aarch64-multiplatform.gccStdenv.cc
+
+              gawk
+            ] ++ pkgs.linux.nativeBuildInputs);
+        }).env;
+      };
+    });
+}
